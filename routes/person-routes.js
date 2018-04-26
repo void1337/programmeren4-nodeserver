7bacf68c1d7b0d12642fda6b0067af74bc9babc4
jhompesc <jhompesc@avans.nl>
2018-04-26 14:36:48 +0200

    routes added

--- routes/person-routes.js
+++ routes/person-routes.js
@@ -1,3 +1,37 @@
 //
 //Person routes
-///
+//
+
+app.get('/api/person', (req, res, next) => {
+	console.log('get was called')
+
+	res.status(200).json(personlist).end()
+})
+
+app.get('/api/person/:id', (req, res, next) => {
+	console.log('get was called')
+	const id = req.params.id
+
+	if(id >= 0 && id < personlist.length){
+		// als id geldig is in de personlist: stuur person terug
+		res.status(200).json(personlist[id]).end()
+	} else {
+		// als id niet geldig is: error
+		const error = {
+			error: 'ID does not exist (index out of bounds)'
+		}
+		next(error)
+	}
+})
+
+app.post('/api/person', (req, res, next) => {
+	console.log('post was called')
+	console.log(req.body)
+
+	const firstname = req.body.firstname
+	const lastname = req.body.lastname
+	const person = new Person(firstname, lastname)
+	personlist.push(person)
+
+	res.status(200).json(person).end()
+})
--- server.js
+++ server.js
@@ -21,39 +21,7 @@ app.use('*', (req, res, next) => {
 	next()
 })
 
-app.get('/api/person', (req, res, next) => {
-	console.log('get was called')
 
-	res.status(200).json(personlist).end()
-})
-
-app.get('/api/person/:id', (req, res, next) => {
-	console.log('get was called')
-	const id = req.params.id
-
-	if(id >= 0 && id < personlist.length){
-		// als id geldig is in de personlist: stuur person terug
-		res.status(200).json(personlist[id]).end()
-	} else {
-		// als id niet geldig is: error
-		const error = {
-			error: 'ID does not exist (index out of bounds)'
-		}
-		next(error)
-	}
-})
-
-app.post('/api/person', (req, res, next) => {
-	console.log('post was called')
-	console.log(req.body)
-
-	const firstname = req.body.firstname
-	const lastname = req.body.lastname
-	const person = new Person(firstname, lastname)
-	personlist.push(person)
-
-	res.status(200).json(person).end()
-})
 
 // Wanneer de gevraagde endpoint niet gevonden is komen we hier.
 app.use('*', (req, res, next) => {
